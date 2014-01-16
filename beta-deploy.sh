426dea421effe0278190098c9e64d460f06cfdd5
Amanda Cameron <git@amanda.darkdna.net>
2014-01-15 20:51:48 -0500

    Remove beta-deploy stuff, as it's not obsolete.

--- beta-deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#scp -r . ryujin:/srv/ac-get.darkdna.net/beta/
-
-ssh ryujin "cd /srv/ac-get.darkdna.net/; rm -rf beta/; git clone https://git.darkdna.net/amanda/ac-get.git beta; sed -e 's#/repo#/beta/repo#' -i beta/install.manifest; sed -e 's#darkdna\.net/#darkdna\.net/beta#' -i beta/install.lua"
-
-notify-send "Beta Deploy" "Deploy complete."
