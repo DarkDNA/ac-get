5644c7bcf54b3cab5fd74e9bb70f0c1df915c3b7
Amanda Cameron <git@amanda.darkdna.net>
2014-04-16 00:07:30 -0400

    Inital commit of the restructuring of ac-get to /ac-get

--- repo/ac-get/lib/acg/acg.lua
+++ repo/ac-get/lib/acg/acg.lua
@@ -1,31 +1,21 @@
 VERSION = 0
 
-local default_dirs = {
-	["binaries"] = "/bin",
-	["libraries"] = "/lib",
-	["config"] = "/cfg",
-	["startup"] = "/cfg/startup.d",
-	["docs"] = "/docs/",
-	-- ac-get stuff.
-	["state"] = "/lib/ac-get",
-	["repo-state"] = "/lib/ac-get/repos"
-}
-dirs = default_dirs
+dirs = {}
 
-if fs.exists('/ac-get-dirs') then
-	local f = fs.open('/ac-get-dirs', 'r')
-
-	if f ~= nil then
-		for k, v in pairs(textutils.unserialize(f.readAll())) do
-			dirs[k] = v
-		end
-
-		f.close()
-	end
+if not fs.exists('__LIB__/acg/dirs') then
+	error("Invalid state.")
 end
 
+local f = fs.open('__LIB__/acg/dirs', 'r')
+
+for k, v in pairs(textutils.unserialize(f.readAll())) do
+	dirs[k] = v
+end
+
+f.close()
+
 for _, fname in ipairs(fs.list(dirs['libraries'] .. '/acg/')) do
-	if fname ~= 'acg' then
+	if fname ~= 'acg' and fname ~= 'dirs' then
 		dofile(dirs['libraries'] .. '/acg/' .. fname)
 	end
 end
